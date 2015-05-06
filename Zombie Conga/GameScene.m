36ac99c7e7d175dcc31715160e37dda0d0c78527
Iyad Horani <i3d@ironic3d.com>
2015-05-06 20:01:48 +1000

    Add math helper methods

--- Zombie Conga/GameScene.m
+++ Zombie Conga/GameScene.m
@@ -10,6 +10,34 @@
 
 static const float ZOMBIE_MOVE_POINTS_PER_SEC = 120.0;
 
+static inline CGPoint CGPointAdd(const CGPoint a,
+                                const CGPoint b) {
+    return CGPointMake(a.x + b.x, a.y + b.y);
+}
+
+static inline CGPoint CGPointSubtract(const CGPoint a,
+                                     const CGPoint b) {
+    return CGPointMake(a.x - b.x, a.y - b.y);
+}
+
+static inline CGPoint CGPointMultiplyScalar(const CGPoint a,
+                                            const CGFloat b) {
+    return CGPointMake(a.x * b, a.y * b);
+}
+
+static inline CGFloat CGPointLength(const CGPoint a) {
+    return sqrtf(a.x * a.x + a.y * a.y);
+}
+
+static inline CGPoint CGPoinNormalize(const CGPoint a) {
+    CGFloat length = CGPointLength(a);
+    return CGPointMake(a.x / length, a.y / length);
+}
+
+static inline CGFloat CGPointToAngle(const CGPoint a) {
+    return atan2f(a.y, a.y);
+}
+
 @implementation GameScene {
     SKSpriteNode *_zombie;
     NSTimeInterval _lastUpdateTime;

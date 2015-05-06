2b525dca91163524f92b5358f6a7116ae0963620
Iyad Horani <i3d@ironic3d.com>
2015-05-06 19:40:21 +1000

    Make zombie move based on delta time

--- Zombie Conga/GameScene.m
+++ Zombie Conga/GameScene.m
@@ -8,13 +8,50 @@
 
 #import "GameScene.h"
 
-@implementation GameScene
+static const float ZOMBIE_MOVE_POINTS_PER_SEC = 120.0;
+
+@implementation GameScene {
+    SKSpriteNode *_zombie;
+    NSTimeInterval _lastUpdateTime;
+    NSTimeInterval _dt;
+    CGPoint _velocity;
+}
 
 -(instancetype)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size]) {
         self.backgroundColor = [SKColor whiteColor];
+        
+        SKSpriteNode *bg = [SKSpriteNode spriteNodeWithImageNamed:@"background"];
+        bg.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+        [self addChild:bg];
+        
+        _zombie = [SKSpriteNode spriteNodeWithImageNamed:@"zombie1"];
+        _zombie.position = CGPointMake(100, 100);
+        [self addChild:_zombie];
     }
     return self;
+}
+
+-(void)update:(NSTimeInterval)currentTime {
+    if (_lastUpdateTime) {
+        _dt = currentTime - _lastUpdateTime;
+    } else {
+        _dt = 0;
+    }
+    _lastUpdateTime = currentTime;
+    [self moveSprite:_zombie velocity:CGPointMake(ZOMBIE_MOVE_POINTS_PER_SEC, 0)];
+}
+
+#pragma mark USER METHODS
+
+-(void)moveSprite:(SKSpriteNode *)sprite
+         velocity:(CGPoint)velocity {
+    
+    CGPoint amountToMove = CGPointMake(velocity.x * _dt,
+                                       velocity.y * _dt);
+    
+    sprite.position = CGPointMake(sprite.position.x + amountToMove.x,
+                                  sprite.position.y + amountToMove.y);
 }
 
 @end
